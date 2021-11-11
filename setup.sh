07cc82bbb302eb8b3fa9631f05b531de9ef5c1e1
rishabh-modi2 <88582334+rishabh-modi2@users.noreply.github.com>
2021-11-11 15:02:52 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,5 +5,3 @@ wget -q https://downloads.rclone.org/rclone-current-linux-amd64.zip
 unzip -qqj rclone-current-linux-amd64.zip
 rm -rf rclone-current-linux-amd64.zip
 export PATH=$PWD/:$PATH
-
-apt-get --quiet --quiet --yes --print-uris install aria2c| cut --delimiter ' ' --fields 1 | xargs wget
